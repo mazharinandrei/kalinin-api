7de23f53d119ebe49f125b2a3310d72949ff835f
mazharinandrei <79073599+mazharinandrei@users.noreply.github.com>
2025-11-17 21:14:41 +0700

    v1 prefix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 from api.v1.universal_replies import router as universal_router_v1
 from api.v1.rules import router as rules_router_v1
@@ -7,9 +7,13 @@ from api.v1.process_message import router as reply_router_v1
 
 app = FastAPI(title="🤖Kalinin API")
 
-app.include_router(reply_router_v1)
-app.include_router(rules_router_v1)
-app.include_router(universal_router_v1)
+router_v1 = APIRouter(prefix="/v1")
+
+router_v1.include_router(reply_router_v1)
+router_v1.include_router(rules_router_v1)
+router_v1.include_router(universal_router_v1)
+
+app.include_router(router_v1)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
