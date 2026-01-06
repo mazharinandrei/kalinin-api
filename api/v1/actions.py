252f952f342ed7df0193db62513fbe3b1bdb5b6a
mazharinandrei <79073599+mazharinandrei@users.noreply.github.com>
2026-01-06 16:19:06 +0700

    feat: add roll_dice endpoint

--- api/v1/actions.py
+++ api/v1/actions.py
@@ -3,7 +3,8 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from api.v1.api_spec import ProcessMessageSpec
-from dependencies import reply_service
+from dependencies import reply_service, roll_dice_service
+from domain.roll_dice.roll_dice_service import RollDiceService
 from domain.rule_based_replies.rule_based_reply_service import ReplyService
 from schemas.message import Message
 
@@ -21,3 +22,12 @@ async def process_message(
 ) -> dict:
     reply = await service.get_reply(input_message=str(message.text))
     return {"data": reply}
+
+
+@router.get("/roll_dice")
+async def roll_dice(
+    service: Annotated[RollDiceService, Depends(roll_dice_service)],
+    user_id: str | None = None,
+) -> dict:
+    roll = await service.roll()
+    return {"data": roll}
