55234e16a13c6e4266e045e06dc41879de4e2a2e
mazharinandrei <79073599+mazharinandrei@users.noreply.github.com>
2026-01-06 16:15:58 +0700

    fead: add RollDiceService

---
+++ domain/roll_dice/roll_dice_service.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from random import randint, choice
+
+from domain.crud_service import CRUDService
+from domain.enities.roll_dice_result import RollDiceResultEntity
+
+
+@dataclass
+class RollDiceService(CRUDService):
+    async def roll(self) -> RollDiceResultEntity:
+        async with self.uow.begin() as session:
+            rolled_dice_value = randint(1, 20)
+            result = await self.repository.get_roll_interpretation(
+                session=session, value=rolled_dice_value
+            )
+            result = choice(result)
+            return RollDiceResultEntity(
+                rolled_dice_value=rolled_dice_value, text=result.input_text
+            )
